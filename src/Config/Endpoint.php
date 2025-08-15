24218a8aa1ff8d74d8582fb5bdc75173cc15572b
m0npay <m0npay1945@gmail.com>
2025-08-15 13:29:42 +0700

    Update payment API URL in Endpoint class

--- src/Config/Endpoint.php
+++ src/Config/Endpoint.php
@@ -5,7 +5,7 @@ namespace Payment\Config;
 class Endpoint
 {
     public const URL_PAYMENT_PAGE = 'https://payment-page.nusapay.co.id/';
-    public const URL_PAYMENT_API = 'http://127.0.0.1:9501/';
+    public const URL_PAYMENT_API = 'https://api-payment-page.um0phbmlhwn0qyay58pf.click/';
 
     public const PAYMENTS = '/payments';
     public const TOKEN_GENERATE = '/api/token/generate';
